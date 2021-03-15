7861c29d09aa4cdc8dfd2991e8e8cbee6d0bafeb
youme <mrsimplelife@naver.com>
2021-03-15 21:29:44 +0900

    닫기 버튼

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -8,6 +9,11 @@ export class BaseComponent<T extends HTMLElement> implements Component {
     const template = document.createElement("template");
     template.innerHTML = htmlString;
     this.element = template.content.firstElementChild! as T;
+  }
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement)
+      throw new Error("Parent mismatch!");
+    parent.removeChild(this.element);
   }
   attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
     parent.insertAdjacentElement(position, this.element);
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,22 +3,31 @@ import { BaseComponent, Component } from "../component.js";
 export interface Composable {
   addChild(child: Component): void;
 }
+type OncloseListener = () => void;
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable {
+  private closeListener?: OncloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body"></section>
             <div class="page-item__controls">
-              <button class="clode">⨂</button>
+              <button class="close">⨂</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
       ".page-item__body"
     )! as HTMLElement;
     child.attachTo(container, "afterbegin");
+  }
+  setOncloseListner(listener: OncloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -33,5 +42,6 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOncloseListner(() => item.removeFrom(this.element));
   }
 }
